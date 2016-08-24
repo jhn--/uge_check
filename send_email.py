9bf877ed708109567d4ffdfbf8c92c1f4842b995
John <jhn--@users.noreply.github.com>
2016-08-24 17:38:47 +0800

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+
+recipients = ['user1@example.com', 'user2@example.com']
+sender = 'sender@example.com'
+
+datetimenow = str(datetime.datetime.now())
+msg = MIMEMultipart('alternative')
+msg['From'] = sender
+msg['To'] = ", ".join(recipients)
+
+
+def ooc_email():
+    msg['Subject'] = "Nodes down {0}".format(datetimenow)
+
+    nooc = '/var/www/html/initializr/nodes_out_of_circulation.html'
+
+    try:
+        nooc_fp =  open(nooc, 'r')
+    except Exception, e:
+        print(e)
+    else:
+        html_source = "\n".join(line for line in nooc_fp.readlines())
+    finally:
+        nooc_fp.close()
+
+    html_msg = MIMEText(html_source, 'html')
+    msg.attach(html_msg)
+
+    smtpserver = smtplib.SMTP('localhost')
+    smtpserver.sendmail(sender, recipients, msg.as_string())
+    smtpserver.quit()
+
+def main():
+    print()
+
+if __name__ == '__main__':
+    main()
