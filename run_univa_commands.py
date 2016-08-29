bf94282e67a4684469ba2b78b336cfbe18dd61f4
John <jhn--@users.noreply.github.com>
2016-08-29 11:43:27 +0800

    Update and rename run_univa_command.py to run_univa_commands.py

--- run_univa_commands.py
+++ run_univa_commands.py
@@ -5,7 +5,7 @@
 import subprocess
 import logging
 import shlex
-from uge_environ import add_environment_vars
+from univa_environ import add_environment_vars
 
 LOG = logging.getLogger("")
 logfile = "/var/log/messages" 
@@ -16,24 +16,23 @@ def run_univa_commands(univa_command):
     """ Run UNIVA commands.
     Expects a set, [0] is the command. [1] is the output path for the XML file."""
 
-    with open(univa_command[1], 'w') as univa_output_fp:
-        LOG.info("run_univa_commands : Getting `{0}` results - Trying".format(univa_command[0]))
-        try:
+    # If the environment variables for UNIVA has been set, proceed with running UNIVA commands.
+    if (add_environment_vars() == True):
+        with open(univa_command[1], 'w') as univa_output_fp:
+            LOG.info("run_univa_commands : Getting `{0}` results - Trying".format(univa_command[0]))
             univa_command_result = subprocess.Popen(shlex.split(univa_command[0]), stdout = univa_output_fp)
-        except Exception, e:
-            # Unable to get results for {univa_command}.
-            LOG.fatal("run_univa_commands : Command - '%s' - raised error: returncode: %s, output: %s" % (e.cmd, e.returncode, e.output))
-            LOG.fatal("run_univa_commands : Unable to get results for `{0}`. Maybe qmaster is down?".format(univa_command[0]))
-        else:
-            univa_output_fp.close()
             LOG.info("`run_univa_commands : {0}` results have been written.".format(univa_command[0]))
-
-    return True
+        return True
+    else:
+        LOG.info("run_univa_commands : add_environment_vars failed.")
 
 def main():
     """ Main function, you're not supposed to call this function if you import this script from check_uge.py, call run_univa_commands() directly, the following lines of code serves as an example on what sort of datatype to pass into run_univa_commands(). """
 
-    dictofugecommands = {"qhost -q -xml": "/usr/local/bin/scripts/qhost-q_xml.xml", "qstat -u \'*\' -xml": "/usr/local/bin/scripts/qstat_all_users.xml"}
+    dictofugecommands = {
+    "qhost -q -xml": "/usr/local/bin/scripts/qhost-q_xml.xml",
+    "qstat -u \'*\' -xml": "/usr/local/bin/scripts/qstat_all_users.xml"
+    }
 
     for ugecommand in dictofugecommands.iteritems():
         run_univa_commands(ugecommand)
